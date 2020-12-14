151e7098756b2ec17fdcadc9bc8a2bc298ca9055
ziouosan <ziouosan@gmail.com>
2020-12-14 19:31:05 +0100

    Setup projects boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 
-const commonConfiguration = require("./webpapck/common");
+const commonConfiguration = require("./webpack/common");
 
 module.exports = (_env, { mode }) => {
   const properlyConfig = require(`./webpack/${mode}`);
